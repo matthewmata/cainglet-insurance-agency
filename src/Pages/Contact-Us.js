658a910d1ec440feaa72182ce2d03288276f5bc1
Matthew Mata <matthewmata1030@gmail.com>
2021-09-30 15:00:02 -0700

    fixing ringy bug

--- src/Pages/Contact-Us.js
+++ src/Pages/Contact-Us.js
@@ -28,23 +28,23 @@ const ContactUs = ({ width }) => {
       email,
     });
 
-    const netlifyResponse = await fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": "contact-us",
-        name,
-        phone,
-        email,
-        interestIn,
-        comments,
-      }),
-    })
+    // const netlifyResponse = await fetch("/", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //   body: encode({
+    //     "form-name": "contact-us",
+    //     name,
+    //     phone,
+    //     email,
+    //     interestIn,
+    //     comments,
+    //   }),
+    // })
 
     console.log('rr', ringyResponse);
-    console.log('nr',netlifyResponse);
+    // console.log('nr',netlifyResponse);
 
-    if (ringyResponse.status === 200 && netlifyResponse.status === 200) {
+    if (ringyResponse.status === 200) {
       setSubmit(true);
     }
   };
